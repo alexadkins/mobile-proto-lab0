a351af28239648ac8f0ac2bda3abdb23efc675f2
Alex Adkins <adkins.alexandra@gmail.com>
2014-09-10 21:40:05 -0400

    Finished Lab 0

--- app/src/main/java/com/alexadkins/mobileprotoday1/MainActivity.java
+++ app/src/main/java/com/alexadkins/mobileprotoday1/MainActivity.java
@@ -4,16 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
     //Keep this as clean as possible!
     //This starts when your app opens
 
+    boolean clicked = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.my_text_view);
+
+        Button myButton = (Button) findViewById(R.id.my_button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(getString(clicked ? R.string.different_string : R.string.different_string2));
+                clicked ^= true;
+            }
+        });
     }
 
 
